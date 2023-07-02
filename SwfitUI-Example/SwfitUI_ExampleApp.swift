cf7151e3f9c4dce2e50875ce39ca7707dd6fcd6b
Pranav Choudhary <pranavchoudhary500@gmail.local>
2023-07-02 11:46:10 +0530

    Initial Commit

---
+++ SwfitUI-Example/SwfitUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI_ExampleApp.swift
+//  SwfitUI-Example
+//
+//  Created by Pranav Choudhary on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
